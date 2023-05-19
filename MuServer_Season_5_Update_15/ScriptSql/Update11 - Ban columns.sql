4b930cccd94339e502ea49193b579c6e3c1f7308
sven-n <sven-n@users.noreply.github.com>
2023-05-19 12:57:06 +0200

    Removed server files

--- MuServer_Season_5_Update_15/ScriptSql/Update11 - Ban columns.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE CHARACTER ADD Bloc_Expire smalldatetime
-ALTER TABLE MEMB_INFO ADD Bloc_Expire smalldatetime
--- Source MuServer Update 15/GameServer/GameServer/HackCheck.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void DecryptData(BYTE* lpMsg,int size);
-void EncryptData(BYTE* lpMsg,int size);
-void MHPDecryptData(BYTE* lpMsg,int size);
-void MHPEncryptData(BYTE* lpMsg,int size);
-void InitHackCheck();
