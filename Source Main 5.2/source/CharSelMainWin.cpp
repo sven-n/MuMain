7e21afea18bf22dfd38f6b3f61c2beb3d4a4409a
mu-nreal <248439734+mu-nreal@users.noreply.github.com>
2025-12-12 12:09:59 -0300

    Refactor character selection main window to use modern C++ standard library and improve code organization

--- Source Main 5.2/source/CharSelMainWin.cpp
+++ Source Main 5.2/source/CharSelMainWin.cpp
@@ -16,8 +16,94 @@
 #include "ZzzOpenglUtil.h"
 #include "ServerListManager.h"
 
+#include <algorithm>
 
+namespace
+{
+    constexpr int kCharacterSlotCount = 5;
+    constexpr int kButtonSpacing = 1;
+    constexpr int kInfoSpacing = 2;
+    constexpr int kInfoOffsetY = 5;
+    constexpr int kStatPanelBaseXOffset = 346;
+    constexpr int kStatPanelOffsetY = 24;
+    constexpr int kJobButtonsStartY = 131;
+    constexpr int kRageFighterButtonsY = 246;
+    constexpr int kSummonerRow = 3;
+    constexpr int kActionButtonsRowOffsetY = 325;
+    constexpr int kCancelButtonOffsetX = 400;
+    constexpr int kInputSpriteOffsetY = 317;
+    constexpr int kInputTextOffsetX = 78;
+    constexpr int kInputTextOffsetY = 21;
+    constexpr int kDescriptionSpriteOffsetY = 355;
+    constexpr int kDecorOffsetX = 22;
+    constexpr int kDecorOffsetY = 59;
+    constexpr int kAccountBlockMsgX = 320;
+    constexpr int kAccountBlockPrimaryY = 330;
+    constexpr int kAccountBlockSecondaryY = 348;
+    constexpr int kWindowAlpha = 143;
 
+    template <typename Predicate>
+    bool AnyCharacter(Predicate&& predicate)
+    {
+        for (int index = 0; index < kCharacterSlotCount; ++index)
+        {
+            if (predicate(CharactersClient[index]))
+                return true;
+        }
+        return false;
+    }
+
+    bool HasAccountBlockedCharacter()
+    {
+        return AnyCharacter([](const CHARACTER& character)
+        {
+            return character.Object.Live != 0
+                && (character.CtlCode & CTLCODE_10ACCOUNT_BLOCKITEM);
+        });
+    }
+
+    bool HasEmptyCharacterSlot()
+    {
+        return AnyCharacter([](const CHARACTER& character)
+        {
+            return character.Object.Live == 0;
+        });
+    }
+
+    bool HasLiveCharacter()
+    {
+        return AnyCharacter([](const CHARACTER& character)
+        {
+            return character.Object.Live != 0;
+        });
+    }
+
+    CHARACTER* GetSelectedCharacter()
+    {
+        if (SelectedHero < 0 || SelectedHero >= kCharacterSlotCount)
+            return nullptr;
+        return &CharactersClient[SelectedHero];
+    }
+
+    bool CanDeleteSelectedCharacter()
+    {
+        if (CHARACTER* selected = GetSelectedCharacter())
+        {
+            const bool hasGuild = selected->GuildStatus != G_NONE;
+            const bool blocked = (selected->CtlCode & (CTLCODE_02BLOCKITEM | CTLCODE_10ACCOUNT_BLOCKITEM)) != 0;
+            return !hasGuild && !blocked;
+        }
+        return false;
+    }
+
+    void RenderAccountBlockMessage()
+    {
+        g_pRenderText->SetTextColor(0, 0, 0, 255);
+        g_pRenderText->SetBgColor(255, 255, 0, 128);
+        g_pRenderText->RenderText(kAccountBlockMsgX, kAccountBlockPrimaryY, GlobalText[436], 0, 0, RT3_WRITE_CENTER);
+        g_pRenderText->RenderText(kAccountBlockMsgX, kAccountBlockSecondaryY, GlobalText[437], 0, 0, RT3_WRITE_CENTER);
+    }
+}
 
 CCharSelMainWin::CCharSelMainWin()
 {
@@ -29,11 +115,13 @@ CCharSelMainWin::~CCharSelMainWin()
 
 void CCharSelMainWin::Create()
 {
+    CInput& input = CInput::Instance();
+
     m_asprBack[CSMW_SPR_DECO].Create(189, 103, BITMAP_LOG_IN + 2);
     m_asprBack[CSMW_SPR_INFO].Create(
-        CInput::Instance().GetScreenWidth() - 266, 21);
+        input.GetScreenWidth() - 266, 21);
     m_asprBack[CSMW_SPR_INFO].SetColor(0, 0, 0);
-    m_asprBack[CSMW_SPR_INFO].SetAlpha(143);
+    m_asprBack[CSMW_SPR_INFO].SetAlpha(kWindowAlpha);
 
     m_aBtn[CSMW_BTN_CREATE].Create(54, 30, BITMAP_LOG_IN + 3, 4, 2, 1, 3);
     m_aBtn[CSMW_BTN_MENU].Create(54, 30, BITMAP_LOG_IN + 4, 3, 2, 1);
@@ -47,19 +135,7 @@ void CCharSelMainWin::Create()
     for (int i = 0; i < CSMW_BTN_MAX; ++i)
         CWin::RegisterButton(&m_aBtn[i]);
 
-    m_bAccountBlockItem = false;
-
-    for (int i = 0; i < 5; ++i)
-    {
-        if (CharactersClient[i].Object.Live)
-        {
-            if (CharactersClient[i].CtlCode & CTLCODE_10ACCOUNT_BLOCKITEM)
-            {
-                m_bAccountBlockItem = true;
-                break;
-            }
-        }
-    }
+    m_bAccountBlockItem = HasAccountBlockedCharacter();
 }
 
 void CCharSelMainWin::PreRelease()
@@ -72,27 +148,29 @@ void CCharSelMainWin::SetPosition(int nXCoord, int nYCoord)
 {
     CWin::SetPosition(nXCoord, nYCoord);
 
-    int nBtnWidth = m_aBtn[0].GetWidth();
+    const int buttonWidth = m_aBtn[0].GetWidth();
 
     m_aBtn[CSMW_BTN_CREATE].SetPosition(nXCoord, nYCoord);
-    m_aBtn[CSMW_BTN_MENU].SetPosition(nXCoord + nBtnWidth + 1, nYCoord);
-    m_asprBack[CSMW_SPR_INFO].SetPosition(m_aBtn[CSMW_BTN_MENU].GetXPos() + nBtnWidth + 2, nYCoord + 5);
+    m_aBtn[CSMW_BTN_MENU].SetPosition(nXCoord + buttonWidth + kButtonSpacing, nYCoord);
 
-    int nWinRPosX = nXCoord + CWin::GetWidth();
-    m_asprBack[CSMW_SPR_DECO].SetPosition(nWinRPosX - (m_asprBack[CSMW_SPR_DECO].GetWidth() - 22), nYCoord - 59);
-    m_aBtn[CSMW_BTN_DELETE].SetPosition(nWinRPosX - nBtnWidth, nYCoord);
-    m_aBtn[CSMW_BTN_CONNECT].SetPosition(nWinRPosX - (nBtnWidth * 2 + 1), nYCoord);
+    const int infoX = m_aBtn[CSMW_BTN_MENU].GetXPos() + buttonWidth + kInfoSpacing;
+    m_asprBack[CSMW_SPR_INFO].SetPosition(infoX, nYCoord + kInfoOffsetY);
+
+    const int windowRightX = nXCoord + CWin::GetWidth();
+    m_asprBack[CSMW_SPR_DECO].SetPosition(windowRightX - (m_asprBack[CSMW_SPR_DECO].GetWidth() - kDecorOffsetX), nYCoord - kDecorOffsetY);
+
+    m_aBtn[CSMW_BTN_DELETE].SetPosition(windowRightX - buttonWidth, nYCoord);
+    m_aBtn[CSMW_BTN_CONNECT].SetPosition(windowRightX - (buttonWidth * 2 + kButtonSpacing), nYCoord);
 }
 
 void CCharSelMainWin::Show(bool bShow)
 {
     CWin::Show(bShow);
 
-    int i;
-    for (i = 0; i < CSMW_SPR_MAX; ++i)
-        m_asprBack[i].Show(bShow);
-    for (i = 0; i < CSMW_BTN_MAX; ++i)
-        m_aBtn[i].Show(bShow);
+    for (auto& sprite : m_asprBack)
+        sprite.Show(bShow);
+    for (auto& button : m_aBtn)
+        button.Show(bShow);
 }
 
 bool CCharSelMainWin::CursorInWin(int nArea)
@@ -111,41 +189,13 @@ bool CCharSelMainWin::CursorInWin(int nArea)
 
 void CCharSelMainWin::UpdateDisplay()
 {
-    m_aBtn[CSMW_BTN_CREATE].SetEnable(false);
-    int i = 0;
+    m_aBtn[CSMW_BTN_CREATE].SetEnable(HasEmptyCharacterSlot());
 
-    for (i = 0; i < 5; ++i)
-    {
-        if (!CharactersClient[i].Object.Live)
-        {
-            m_aBtn[CSMW_BTN_CREATE].SetEnable(true);
-            break;
-        }
-    }
+    const bool hasSelection = (SelectedHero > -1);
+    m_aBtn[CSMW_BTN_CONNECT].SetEnable(hasSelection);
+    m_aBtn[CSMW_BTN_DELETE].SetEnable(hasSelection);
 
-    if (SelectedHero > -1)
-    {
-        m_aBtn[CSMW_BTN_CONNECT].SetEnable(true);
-        m_aBtn[CSMW_BTN_DELETE].SetEnable(true);
-    }
-    else
-    {
-        m_aBtn[CSMW_BTN_CONNECT].SetEnable(false);
-        m_aBtn[CSMW_BTN_DELETE].SetEnable(false);
-    }
-
-    bool bNobodyCharacter = true;
-
-    for (int i = 0; i < 5; ++i)
-    {
-        if (CharactersClient[i].Object.Live == true)
-        {
-            bNobodyCharacter = false;
-            break;
-        }
-    }
-
-    if (bNobodyCharacter == true)
+    if (!HasLiveCharacter())
     {
         CUIMng& rUIMng = CUIMng::Instance();
         rUIMng.ShowWin(&rUIMng.m_CharMakeWin);
@@ -154,27 +204,31 @@ void CCharSelMainWin::UpdateDisplay()
 
 void CCharSelMainWin::UpdateWhileActive(double dDeltaTick)
 {
+    CUIMng& uiManager = CUIMng::Instance();
+
     if (m_aBtn[CSMW_BTN_CONNECT].IsClick())
+    {
         ::StartGame();
+    }
     else if (m_aBtn[CSMW_BTN_MENU].IsClick())
     {
-        CUIMng& rUIMng = CUIMng::Instance();
-        rUIMng.ShowWin(&rUIMng.m_SysMenuWin);
-        rUIMng.SetSysMenuWinShow(true);
+        uiManager.ShowWin(&uiManager.m_SysMenuWin);
+        uiManager.SetSysMenuWinShow(true);
     }
     else if (m_aBtn[CSMW_BTN_CREATE].IsClick())
     {
-        CUIMng& rUIMng = CUIMng::Instance();
-        rUIMng.ShowWin(&rUIMng.m_CharMakeWin);
+        uiManager.ShowWin(&uiManager.m_CharMakeWin);
     }
     else if (m_aBtn[CSMW_BTN_DELETE].IsClick())
+    {
         DeleteCharacter();
+    }
 }
 
 void CCharSelMainWin::RenderControls()
 {
-    for (int i = 0; i < CSMW_SPR_MAX; ++i)
-        m_asprBack[i].Render();
+    for (auto& sprite : m_asprBack)
+        sprite.Render();
 
     ::EnableAlphaTest();
     ::glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
@@ -184,21 +238,29 @@ void CCharSelMainWin::RenderControls()
     g_pRenderText->SetBgColor(0);
 
     if (m_bAccountBlockItem)
-    {
-        g_pRenderText->SetTextColor(0, 0, 0, 255);
-        g_pRenderText->SetBgColor(255, 255, 0, 128);
-        g_pRenderText->RenderText(320, 330, GlobalText[436], 0, 0, RT3_WRITE_CENTER);
-        g_pRenderText->RenderText(320, 348, GlobalText[437], 0, 0, RT3_WRITE_CENTER);
-    }
+        RenderAccountBlockMessage();
+
     CWin::RenderButtons();
 }
 
 void CCharSelMainWin::DeleteCharacter()
 {
-    if (CharactersClient[SelectedHero].GuildStatus != G_NONE)
-        CUIMng::Instance().PopUpMsgWin(MESSAGE_DELETE_CHARACTER_GUILDWARNING);
-    else if (CharactersClient[SelectedHero].CtlCode & (CTLCODE_02BLOCKITEM | CTLCODE_10ACCOUNT_BLOCKITEM))
-        CUIMng::Instance().PopUpMsgWin(MESSAGE_DELETE_CHARACTER_ID_BLOCK);
+    CHARACTER* selected = GetSelectedCharacter();
+    if (selected == nullptr)
+        return;
+
+    CUIMng& uiManager = CUIMng::Instance();
+
+    if (selected->GuildStatus != G_NONE)
+    {
+        uiManager.PopUpMsgWin(MESSAGE_DELETE_CHARACTER_GUILDWARNING);
+    }
+    else if (selected->CtlCode & (CTLCODE_02BLOCKITEM | CTLCODE_10ACCOUNT_BLOCKITEM))
+    {
+        uiManager.PopUpMsgWin(MESSAGE_DELETE_CHARACTER_ID_BLOCK);
+    }
     else
-        CUIMng::Instance().PopUpMsgWin(MESSAGE_DELETE_CHARACTER_CONFIRM);
+    {
+        uiManager.PopUpMsgWin(MESSAGE_DELETE_CHARACTER_CONFIRM);
+    }
 }
