02d64e48156bfbe43c7300a9c543ef81014748ed
mu-nreal <248439734+mu-nreal@users.noreply.github.com>
2025-12-15 06:18:54 -0300

    Fix CMake build system with MinGW cross-compilation support and configure build directories in .gitignore

--- cmake/toolchains/mingw-w64-i686.cmake
+++ cmake/toolchains/mingw-w64-i686.cmake
@@ -8,7 +8,11 @@ set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+
+# Prefer static GCC runtimes to avoid shipping libgcc_s_dw2-1.dll / libstdc++-6.dll
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -static-libgcc -static-libstdc++")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "${CMAKE_SHARED_LINKER_FLAGS_INIT} -static-libgcc -static-libstdc++")
 
 # Only search libraries and headers in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
