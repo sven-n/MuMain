dbaafe6731472a2fa5e6c5f8b5c248e98480db5e
Mosch0512 <maximilian.schloemer+github@gmail.com>
2026-01-11 21:03:33 +0100

    create skill data handler facade
    - Created DataHandler/SkillData/SkillDataHandler.h/cpp
    - Singleton pattern matching ItemDataHandler architecture
    - Delegates Load() to SkillDataLoader (available in all builds)
    - Delegates Save() and ExportToCsv() to SkillDataSaver/Exporter (editor-only)
    - Provides data access methods: GetSkillAttributes(), GetSkillAttribute(), GetSkillCount()
    - Bounds checking in GetSkillAttribute() returns nullptr for invalid indices
    - Non-copyable singleton with deleted copy constructor and assignment operator
    - Macro g_SkillDataHandler for convenient access
    - Operates on global SkillAttribute array

---
+++ src/source/GameData/SkillData/SkillDataHandler.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "SkillDataHandler.h"
+#include "SkillDataLoader.h"
+#include "GameData/SkillData/SkillStructs.h"
+#include "_struct.h"
+#include "_define.h"
+#include "ZzzInfomation.h"
+
+#ifdef _EDITOR
+#include "SkillDataSaver.h"
+#include "SkillDataExporter.h"
+#endif
+
+// External references
+extern SKILL_ATTRIBUTE* SkillAttribute;
+
+CSkillDataHandler::CSkillDataHandler()
+{
+}
+
+CSkillDataHandler& CSkillDataHandler::GetInstance()
+{
+    static CSkillDataHandler instance;
+    return instance;
+}
+
+SKILL_ATTRIBUTE* CSkillDataHandler::GetSkillAttributes()
+{
+    return SkillAttribute;
+}
+
+SKILL_ATTRIBUTE* CSkillDataHandler::GetSkillAttribute(int index)
+{
+    if (index >= 0 && index < MAX_SKILLS)
+        return &SkillAttribute[index];
+    return nullptr;
+}
+
+int CSkillDataHandler::GetSkillCount() const
+{
+    return MAX_SKILLS;
+}
+
+bool CSkillDataHandler::Load(wchar_t* fileName)
+{
+    return SkillDataLoader::Load(fileName);
+}
+
+#ifdef _EDITOR
+bool CSkillDataHandler::Save(wchar_t* fileName, std::string* outChangeLog)
+{
+    return SkillDataSaver::Save(fileName, outChangeLog);
+}
+
+bool CSkillDataHandler::ExportToCsv(wchar_t* fileName)
+{
+    return SkillDataExporter::ExportToCsv(fileName);
+}
+#endif
