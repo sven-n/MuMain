fb7fcf7f389c0408fabe77d06d020abee4d6b7b6
Mosch0512 <maximilian.schloemer+github@gmail.com>
2026-01-11 12:44:20 +0100

    Refactored change tracking logic in `ItemDataSaver` using X-macros for improved maintainability and reduced redundancy.

--- src/source/DataHandler/ItemData/ItemDataSaver.cpp
+++ src/source/DataHandler/ItemData/ItemDataSaver.cpp
@@ -5,6 +5,7 @@
 #include "ItemDataSaver.h"
 #include "ItemDataFileIO.h"
 #include "GameData/ItemData/ItemStructs.h"
+#include "GameData/ItemData/ItemFieldDefs.h"
 #include "Translation/i18n.h"
 #include "_struct.h"
 #include "_define.h"
@@ -15,6 +16,37 @@
 
 // External references
 extern ITEM_ATTRIBUTE* ItemAttribute;
+
+// X-Macro helpers for change tracking
+#define COMPARE_FIELD_Bool(name, type, arraySize, width) \
+    if (oldItem.name != newItem.name) { \
+        itemChanges << "  " #name ": " << (oldItem.name ? "true" : "false") << " -> " << (newItem.name ? "true" : "false") << "\n"; \
+        changed = true; \
+    }
+
+#define COMPARE_FIELD_Byte(name, type, arraySize, width) \
+    if (oldItem.name != newItem.name) { \
+        itemChanges << "  " #name ": " << (int)oldItem.name << " -> " << (int)newItem.name << "\n"; \
+        changed = true; \
+    }
+
+#define COMPARE_FIELD_Word(name, type, arraySize, width) \
+    if (oldItem.name != newItem.name) { \
+        itemChanges << "  " #name ": " << oldItem.name << " -> " << newItem.name << "\n"; \
+        changed = true; \
+    }
+
+#define COMPARE_FIELD_Int(name, type, arraySize, width) \
+    if (oldItem.name != newItem.name) { \
+        itemChanges << "  " #name ": " << oldItem.name << " -> " << newItem.name << "\n"; \
+        changed = true; \
+    }
+
+#define COMPARE_ARRAY_FIELD(nameWithIndex, baseName, index, type, width) \
+    if (oldItem.baseName[index] != newItem.baseName[index]) { \
+        itemChanges << "  " #nameWithIndex ": " << (int)oldItem.baseName[index] << " -> " << (int)newItem.baseName[index] << "\n"; \
+        changed = true; \
+    }
 
 bool ItemDataSaver::Save(wchar_t* fileName, std::string* outChangeLog)
 {
@@ -90,185 +122,37 @@ bool ItemDataSaver::Save(wchar_t* fileName, std::string* outChangeLog)
 
         CopyItemAttributeToDestination(dest, ItemAttribute[i]);
 
-        // Track changes - now including ALL fields
+        // Track changes using X-macros
         if (originalItems && ItemAttribute[i].Name[0] != 0)
         {
             bool changed = false;
             std::stringstream itemChanges;
 
-            // Name
-            if (wcscmp(originalItems[i].Name, ItemAttribute[i].Name) != 0)
+            ITEM_ATTRIBUTE& oldItem = originalItems[i];
+            ITEM_ATTRIBUTE& newItem = ItemAttribute[i];
+
+            // Name (special case - wide char comparison)
+            if (wcscmp(oldItem.Name, newItem.Name) != 0)
             {
                 char oldName[MAX_ITEM_NAME];
                 char newName[MAX_ITEM_NAME];
-                CMultiLanguage::ConvertToUtf8(oldName, originalItems[i].Name, MAX_ITEM_NAME);
-                CMultiLanguage::ConvertToUtf8(newName, ItemAttribute[i].Name, MAX_ITEM_NAME);
+                CMultiLanguage::ConvertToUtf8(oldName, oldItem.Name, MAX_ITEM_NAME);
+                CMultiLanguage::ConvertToUtf8(newName, newItem.Name, MAX_ITEM_NAME);
                 itemChanges << "  Name: \"" << oldName << "\" -> \"" << newName << "\"\n";
                 changed = true;
             }
 
-            // Basic properties
-            if (originalItems[i].TwoHand != ItemAttribute[i].TwoHand)
-            {
-                itemChanges << "  TwoHand: " << (originalItems[i].TwoHand ? "true" : "false") << " -> " << (ItemAttribute[i].TwoHand ? "true" : "false") << "\n";
-                changed = true;
-            }
-            if (originalItems[i].Level != ItemAttribute[i].Level)
-            {
-                itemChanges << "  Level: " << originalItems[i].Level << " -> " << ItemAttribute[i].Level << "\n";
-                changed = true;
-            }
-            if (originalItems[i].m_byItemSlot != ItemAttribute[i].m_byItemSlot)
-            {
-                itemChanges << "  Slot: " << (int)originalItems[i].m_byItemSlot << " -> " << (int)ItemAttribute[i].m_byItemSlot << "\n";
-                changed = true;
-            }
-            if (originalItems[i].m_wSkillIndex != ItemAttribute[i].m_wSkillIndex)
-            {
-                itemChanges << "  Skill: " << originalItems[i].m_wSkillIndex << " -> " << ItemAttribute[i].m_wSkillIndex << "\n";
-                changed = true;
-            }
+            // All other fields using X-macros
+            #define COMPARE_SIMPLE(name, type, arraySize, width) COMPARE_FIELD_##type(name, type, arraySize, width)
+            ITEM_FIELDS_SIMPLE(COMPARE_SIMPLE)
+            #undef COMPARE_SIMPLE
 
-            // Dimensions
-            if (originalItems[i].Width != ItemAttribute[i].Width)
-            {
-                itemChanges << "  Width: " << (int)originalItems[i].Width << " -> " << (int)ItemAttribute[i].Width << "\n";
-                changed = true;
-            }
-            if (originalItems[i].Height != ItemAttribute[i].Height)
-            {
-                itemChanges << "  Height: " << (int)originalItems[i].Height << " -> " << (int)ItemAttribute[i].Height << "\n";
-                changed = true;
-            }
-
-            // Combat stats
-            if (originalItems[i].DamageMin != ItemAttribute[i].DamageMin)
-            {
-                itemChanges << "  DmgMin: " << (int)originalItems[i].DamageMin << " -> " << (int)ItemAttribute[i].DamageMin << "\n";
-                changed = true;
-            }
-            if (originalItems[i].DamageMax != ItemAttribute[i].DamageMax)
-            {
-                itemChanges << "  DmgMax: " << (int)originalItems[i].DamageMax << " -> " << (int)ItemAttribute[i].DamageMax << "\n";
-                changed = true;
-            }
-            if (originalItems[i].SuccessfulBlocking != ItemAttribute[i].SuccessfulBlocking)
-            {
-                itemChanges << "  Block: " << (int)originalItems[i].SuccessfulBlocking << " -> " << (int)ItemAttribute[i].SuccessfulBlocking << "\n";
-                changed = true;
-            }
-            if (originalItems[i].Defense != ItemAttribute[i].Defense)
-            {
-                itemChanges << "  Def: " << (int)originalItems[i].Defense << " -> " << (int)ItemAttribute[i].Defense << "\n";
-                changed = true;
-            }
-            if (originalItems[i].MagicDefense != ItemAttribute[i].MagicDefense)
-            {
-                itemChanges << "  MagDef: " << (int)originalItems[i].MagicDefense << " -> " << (int)ItemAttribute[i].MagicDefense << "\n";
-                changed = true;
-            }
-            if (originalItems[i].WeaponSpeed != ItemAttribute[i].WeaponSpeed)
-            {
-                itemChanges << "  AtkSpd: " << (int)originalItems[i].WeaponSpeed << " -> " << (int)ItemAttribute[i].WeaponSpeed << "\n";
-                changed = true;
-            }
-            if (originalItems[i].WalkSpeed != ItemAttribute[i].WalkSpeed)
-            {
-                itemChanges << "  WalkSpd: " << (int)originalItems[i].WalkSpeed << " -> " << (int)ItemAttribute[i].WalkSpeed << "\n";
-                changed = true;
-            }
-
-            // Durability
-            if (originalItems[i].Durability != ItemAttribute[i].Durability)
-            {
-                itemChanges << "  Dur: " << (int)originalItems[i].Durability << " -> " << (int)ItemAttribute[i].Durability << "\n";
-                changed = true;
-            }
-            if (originalItems[i].MagicDur != ItemAttribute[i].MagicDur)
-            {
-                itemChanges << "  MagDur: " << (int)originalItems[i].MagicDur << " -> " << (int)ItemAttribute[i].MagicDur << "\n";
-                changed = true;
-            }
-            if (originalItems[i].MagicPower != ItemAttribute[i].MagicPower)
-            {
-                itemChanges << "  MagPow: " << (int)originalItems[i].MagicPower << " -> " << (int)ItemAttribute[i].MagicPower << "\n";
-                changed = true;
-            }
-
-            // Requirements
-            if (originalItems[i].RequireStrength != ItemAttribute[i].RequireStrength)
-            {
-                itemChanges << "  Str: " << originalItems[i].RequireStrength << " -> " << ItemAttribute[i].RequireStrength << "\n";
-                changed = true;
-            }
-            if (originalItems[i].RequireDexterity != ItemAttribute[i].RequireDexterity)
-            {
-                itemChanges << "  Dex: " << originalItems[i].RequireDexterity << " -> " << ItemAttribute[i].RequireDexterity << "\n";
-                changed = true;
-            }
-            if (originalItems[i].RequireEnergy != ItemAttribute[i].RequireEnergy)
-            {
-                itemChanges << "  Ene: " << originalItems[i].RequireEnergy << " -> " << ItemAttribute[i].RequireEnergy << "\n";
-                changed = true;
-            }
-            if (originalItems[i].RequireVitality != ItemAttribute[i].RequireVitality)
-            {
-                itemChanges << "  Vit: " << originalItems[i].RequireVitality << " -> " << ItemAttribute[i].RequireVitality << "\n";
-                changed = true;
-            }
-            if (originalItems[i].RequireCharisma != ItemAttribute[i].RequireCharisma)
-            {
-                itemChanges << "  Cha: " << originalItems[i].RequireCharisma << " -> " << ItemAttribute[i].RequireCharisma << "\n";
-                changed = true;
-            }
-            if (originalItems[i].RequireLevel != ItemAttribute[i].RequireLevel)
-            {
-                itemChanges << "  Lvl: " << originalItems[i].RequireLevel << " -> " << ItemAttribute[i].RequireLevel << "\n";
-                changed = true;
-            }
-
-            // Other attributes
-            if (originalItems[i].Value != ItemAttribute[i].Value)
-            {
-                itemChanges << "  Value: " << (int)originalItems[i].Value << " -> " << (int)ItemAttribute[i].Value << "\n";
-                changed = true;
-            }
-            if (originalItems[i].iZen != ItemAttribute[i].iZen)
-            {
-                itemChanges << "  Zen: " << originalItems[i].iZen << " -> " << ItemAttribute[i].iZen << "\n";
-                changed = true;
-            }
-            if (originalItems[i].AttType != ItemAttribute[i].AttType)
-            {
-                itemChanges << "  AttType: " << (int)originalItems[i].AttType << " -> " << (int)ItemAttribute[i].AttType << "\n";
-                changed = true;
-            }
-
-            // Class requirements
-            for (int c = 0; c < MAX_CLASS; c++)
-            {
-                if (originalItems[i].RequireClass[c] != ItemAttribute[i].RequireClass[c])
-                {
-                    const char* classNames[] = {"DW/SM", "DK/BK", "ELF/ME", "MG", "DL", "SUM", "RF"};
-                    itemChanges << "  " << classNames[c] << ": " << (int)originalItems[i].RequireClass[c] << " -> " << (int)ItemAttribute[i].RequireClass[c] << "\n";
-                    changed = true;
-                }
-            }
-
-            // Resistance
-            for (int r = 0; r < MAX_RESISTANCE + 1; r++)
-            {
-                if (originalItems[i].Resistance[r] != ItemAttribute[i].Resistance[r])
-                {
-                    itemChanges << "  Res[" << r << "]: " << (int)originalItems[i].Resistance[r] << " -> " << (int)ItemAttribute[i].Resistance[r] << "\n";
-                    changed = true;
-                }
-            }
+            ITEM_FIELDS_ARRAYS(COMPARE_ARRAY_FIELD)
 
             if (changed)
             {
                 char itemName[MAX_ITEM_NAME];
-                CMultiLanguage::ConvertToUtf8(itemName, ItemAttribute[i].Name, MAX_ITEM_NAME);
+                CMultiLanguage::ConvertToUtf8(itemName, newItem.Name, MAX_ITEM_NAME);
                 changeLog << "[" << i << "] " << itemName << "\n" << itemChanges.str();
                 changeCount++;
             }
