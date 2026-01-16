61b5827c2bc89c94884a0e80fd511ed9efe655c4
Mosch0512 <maximilian.schloemer+github@gmail.com>
2026-01-16 14:08:53 +0100

    Reduce duplicated code

--- src/source/DataHandler/ItemData/ItemDataSaverLegacy.cpp
+++ src/source/DataHandler/ItemData/ItemDataSaverLegacy.cpp
@@ -4,6 +4,7 @@
 
 #include "ItemDataSaverLegacy.h"
 #include "ItemDataFileIO.h"
+#include "GameData/ItemData/ItemAttributeHelpers.h"
 #include "_struct.h"
 #include "_define.h"
 #include "ZzzInfomation.h"
@@ -11,40 +12,6 @@
 
 // External references
 extern ITEM_ATTRIBUTE* ItemAttribute;
-
-// Helper template to copy from runtime structure to legacy file structure
-template<typename TDest>
-static void CopyItemAttributeToLegacyDestination(TDest& dest, ITEM_ATTRIBUTE& source)
-{
-    CMultiLanguage::ConvertToUtf8(dest.Name, source.Name, sizeof(dest.Name));
-    dest.TwoHand = source.TwoHand;
-    dest.Level = source.Level;
-    dest.m_byItemSlot = source.m_byItemSlot;
-    dest.m_wSkillIndex = source.m_wSkillIndex;
-    dest.Width = source.Width;
-    dest.Height = source.Height;
-    dest.DamageMin = source.DamageMin;
-    dest.DamageMax = source.DamageMax;
-    dest.SuccessfulBlocking = source.SuccessfulBlocking;
-    dest.Defense = source.Defense;
-    dest.MagicDefense = source.MagicDefense;
-    dest.WeaponSpeed = source.WeaponSpeed;
-    dest.WalkSpeed = source.WalkSpeed;
-    dest.Durability = source.Durability;
-    dest.MagicDur = source.MagicDur;
-    dest.MagicPower = source.MagicPower;
-    dest.RequireStrength = source.RequireStrength;
-    dest.RequireDexterity = source.RequireDexterity;
-    dest.RequireEnergy = source.RequireEnergy;
-    dest.RequireVitality = source.RequireVitality;
-    dest.RequireCharisma = source.RequireCharisma;
-    dest.RequireLevel = source.RequireLevel;
-    dest.Value = source.Value;
-    dest.iZen = source.iZen;
-    dest.AttType = source.AttType;
-    memcpy(dest.RequireClass, source.RequireClass, sizeof(dest.RequireClass));
-    memcpy(dest.Resistance, source.Resistance, sizeof(dest.Resistance));
-}
 
 bool ItemDataSaverLegacy::SaveLegacy(wchar_t* fileName)
 {
@@ -65,7 +32,7 @@ bool ItemDataSaverLegacy::SaveLegacy(wchar_t* fileName)
         ITEM_ATTRIBUTE_FILE_LEGACY dest;
         memset(&dest, 0, Size);
 
-        CopyItemAttributeToLegacyDestination(dest, ItemAttribute[i]);
+        CopyItemAttributeToDestination(dest, ItemAttribute[i]);
 
         memcpy(pSeek, &dest, Size);
         pSeek += Size;
