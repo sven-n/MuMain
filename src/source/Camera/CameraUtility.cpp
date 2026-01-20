4f17a1c665fee39affbfb77093df4ad1e1e533b4
Mosch0512 <maximilian.schloemer+github@gmail.com>
2026-01-21 00:32:22 +0100

    Refactor `CameraUtility` by modularizing camera logic into static functions (`CalculateCameraViewFar`, `AdjustHeroHeight`, `CalculateCameraPosition`, `SetCameraAngle`, `UpdateCustomCameraDistance`, `UpdateCameraDistance`, `SetCameraFOV`, `HandleEditorMode`), improving code readability, maintainability, and adding detailed function documentation.

--- src/source/Camera/CameraUtility.cpp
+++ src/source/Camera/CameraUtility.cpp
@@ -27,6 +27,9 @@ extern float CameraDistance;
 extern float Camera3DFov;
 extern bool Camera3DRoll;
 
+/**
+ * @brief Calculates camera view distance based on scene and world settings.
+ */
 static float CalculateCameraViewFar(int sceneFlag)
 {
     if (battleCastle::InBattleCastle2(Hero->Object.Position))
@@ -78,228 +81,165 @@ static float CalculateCameraViewFar(int sceneFlag)
     }
 }
 
-bool MoveMainCamera()
+/**
+ * @brief Adjusts hero height based on terrain and mount status.
+ */
+static void AdjustHeroHeight()
 {
-    bool bLockCamera = false;
-
-    if (
-        gMapManager.WorldActive == WD_73NEW_LOGIN_SCENE
-        && CCameraMove::GetInstancePtr()->IsTourMode())
+    if (gMapManager.InChaosCastle() == false || !Hero->Object.m_bActionStart)
     {
-        CameraFOV = 65.0f;
-    }
-    else
-    {
-        CameraFOV = 30.f;
-    }
-
-#ifdef ENABLE_EDIT2
-    {
-        bool EditMove = false;
-        if (!g_pUIManager->IsInputEnable())
+        // Skip height adjustment for Kanturu 3rd when action has started
+        if (gMapManager.WorldActive == WD_39KANTURU_3RD && Hero->Object.m_bActionStart)
         {
-            if (HIBYTE(GetAsyncKeyState(VK_INSERT)) == 128)
-                CameraAngle[2] += 15;
-            if (HIBYTE(GetAsyncKeyState(VK_DELETE)) == 128)
-                CameraAngle[2] -= 15;
-            if (HIBYTE(GetAsyncKeyState(VK_HOME)) == 128)
-                CameraAngle[2] = -45;
-
-            CameraAngle[2] = fmodf(CameraAngle[2] + 360.0f, 360.0f) - 360.0f;
-
-
-            vec3_t p1, p2;
-            Vector(0.f, 0.f, 0.f, p1);
-            FLOAT Velocity = TERRAIN_SCALE * 1.25f * FPS_ANIMATION_FACTOR;
-
-            struct KeyMapping {
-                int vKey;
-                float x;
-                float y;
-            };
-
-            const KeyMapping keyMappings[] = {
-                {VK_LEFT,  -Velocity, -Velocity},
-                {VK_RIGHT,  Velocity,  Velocity},
-                {VK_UP,    -Velocity,  Velocity},
-                {VK_DOWN,   Velocity, -Velocity}
-            };
-
-            for (const auto& mapping : keyMappings)
-            {
-                if (HIBYTE(GetAsyncKeyState(mapping.vKey)) == 128)
-                {
-                    Vector(mapping.x, mapping.y, 0.f, p1);
-                    EditMove = true;
-                    break;
-                }
-            }
-
-            glPushMatrix();
-            glLoadIdentity();
-            glRotatef(-CameraAngle[2], 0.f, 0.f, 1.f);
-            float Matrix[3][4];
-            GetOpenGLMatrix(Matrix);
-            glPopMatrix();
-            VectorRotate(p1, Matrix, p2);
-            VectorAdd(Hero->Object.Position, p2, Hero->Object.Position);
+            // No-op: Hero maintains current Z position during action in Kanturu 3rd
         }
-
-        if (gMapManager.InChaosCastle() == false || !Hero->Object.m_bActionStart)
+        else if (gMapManager.WorldActive == -1 || Hero->Helper.Type != MODEL_HORN_OF_DINORANT || Hero->SafeZone)
         {
-            // Skip height adjustment for Kanturu 3rd when action has started
-            if (gMapManager.WorldActive == WD_39KANTURU_3RD && Hero->Object.m_bActionStart)
-            {
-                // No-op: Hero maintains current Z position during action in Kanturu 3rd
-            }
+            Hero->Object.Position[2] = RequestTerrainHeight(Hero->Object.Position[0], Hero->Object.Position[1]);
+        }
+        else
+        {
+            float baseHeight = RequestTerrainHeight(Hero->Object.Position[0], Hero->Object.Position[1]);
+            if (gMapManager.WorldActive == WD_8TARKAN || gMapManager.WorldActive == WD_10HEAVEN)
+                Hero->Object.Position[2] = baseHeight + 90.f;
             else
-                if (gMapManager.WorldActive == -1 || Hero->Helper.Type != MODEL_HORN_OF_DINORANT || Hero->SafeZone)
-                {
-                    Hero->Object.Position[2] = RequestTerrainHeight(Hero->Object.Position[0], Hero->Object.Position[1]);
-                }
-                else
-                {
-                    if (gMapManager.WorldActive == WD_8TARKAN || gMapManager.WorldActive == WD_10HEAVEN)
-                        Hero->Object.Position[2] = RequestTerrainHeight(Hero->Object.Position[0], Hero->Object.Position[1]) + 90.f;
-                    else
-                        Hero->Object.Position[2] = RequestTerrainHeight(Hero->Object.Position[0], Hero->Object.Position[1]) + 30.f;
-                }
-        }
-
-        if (EditMove)
-        {
-            BYTE PathX[1];
-            BYTE PathY[1];
-            PathX[0] = (BYTE)(Hero->Object.Position[0] / TERRAIN_SCALE);
-            PathY[0] = (BYTE)(Hero->Object.Position[1] / TERRAIN_SCALE);
-
-            SendCharacterMove(Hero->Key, Hero->Object.Angle[2], 1, PathX, PathY, PathX[0], PathY[0]);
-
-            Hero->Path.PathNum = 0;
+                Hero->Object.Position[2] = baseHeight + 30.f;
         }
     }
-#endif //ENABLE_EDIT2
+}
 
-    CameraAngle[0] = 0.f;
-    CameraAngle[1] = 0.f;
+/**
+ * @brief Calculates camera position based on scene and world state.
+ */
+static void CalculateCameraPosition(vec3_t outCameraPosition)
+{
+    int iIndex = TERRAIN_INDEX((Hero->PositionX), (Hero->PositionY));
+    vec3_t Position, TransformPosition;
+    float Matrix[3][4];
 
-    if (CameraTopViewEnable)
+    CameraViewFar = CalculateCameraViewFar(SceneFlag);
+
+    Vector(0.f, -CameraDistance, 0.f, Position);
+    AngleMatrix(CameraAngle, Matrix);
+    VectorIRotate(Position, Matrix, TransformPosition);
+
+    if (SceneFlag == MAIN_SCENE)
     {
-        CameraViewFar = 3200.f;
-        //CameraViewFar = 60000.f;
-        CameraPosition[0] = Hero->Object.Position[0];
-        CameraPosition[1] = Hero->Object.Position[1];
-        CameraPosition[2] = CameraViewFar;
+        g_pCatapultWindow->GetCameraPos(Position);
+    }
+    else if (CCameraMove::GetInstancePtr()->IsTourMode())
+    {
+        CCameraMove::GetInstancePtr()->UpdateTourWayPoint();
+        CCameraMove::GetInstancePtr()->GetCurrentCameraPos(Position);
+        CameraViewFar = 390.f * CCameraMove::GetInstancePtr()->GetCurrentCameraDistanceLevel();
+    }
+
+    if (g_Direction.IsDirection() && !g_Direction.m_bDownHero)
+    {
+        Hero->Object.Position[2] = 300.0f;
+        g_shCameraLevel = g_Direction.GetCameraPosition(Position);
+    }
+    else if (gMapManager.IsPKField() || IsDoppelGanger2())
+    {
+        g_shCameraLevel = 5;
+    }
+    else if (IsDoppelGanger1())
+    {
+        g_shCameraLevel = 5;
+    }
+    else g_shCameraLevel = 0;
+
+    if (CCameraMove::GetInstancePtr()->IsTourMode())
+    {
+        vec3_t temp = { 0.0f, 0.0f, -100.0f };
+        VectorAdd(TransformPosition, temp, TransformPosition);
+    }
+
+    VectorAdd(Position, TransformPosition, outCameraPosition);
+
+    if (gMapManager.InBattleCastle())
+    {
+        outCameraPosition[2] = 255.f;
+    }
+    else if (!CCameraMove::GetInstancePtr()->IsTourMode())
+    {
+        outCameraPosition[2] = Hero->Object.Position[2];
+    }
+
+    if ((TerrainWall[iIndex] & TW_HEIGHT) == TW_HEIGHT)
+    {
+        outCameraPosition[2] = g_fSpecialHeight = 1200.f + 1;
+    }
+    outCameraPosition[2] += CameraDistance - 150.f;
+
+    // Apply custom camera distance for special terrain
+    if (g_fCameraCustomDistance != 0.f)
+    {
+        vec3_t angle = { 0.f, 0.f, -45.f };
+        Vector(0.f, g_fCameraCustomDistance, 0.f, Position);
+        AngleMatrix(angle, Matrix);
+        VectorIRotate(Position, Matrix, TransformPosition);
+        VectorAdd(outCameraPosition, TransformPosition, outCameraPosition);
+    }
+}
+
+/**
+ * @brief Sets camera angle based on scene and tour mode.
+ */
+static void SetCameraAngle()
+{
+    if (CCameraMove::GetInstancePtr()->IsTourMode())
+    {
+        CCameraMove::GetInstancePtr()->SetAngleFrustum(-112.5f);
+        CameraAngle[0] = CCameraMove::GetInstancePtr()->GetAngleFrustum();
+        CameraAngle[1] = 0.0f;
+        CameraAngle[2] = CCameraMove::GetInstancePtr()->GetCameraAngle();
+    }
+    else if (SceneFlag == CHARACTER_SCENE)
+    {
+        CameraAngle[0] = -84.5f;
+        CameraAngle[1] = 0.0f;
+        CameraAngle[2] = -75.0f;
+        CameraPosition[0] = 9758.93f;
+        CameraPosition[1] = 18913.11f;
+        CameraPosition[2] = 675.5f;
     }
     else
     {
-        int iIndex = TERRAIN_INDEX((Hero->PositionX), (Hero->PositionY));
-        vec3_t Position, TransformPosition;
-        float Matrix[3][4];
+        CameraAngle[0] = -48.5f;
+    }
 
-        CameraViewFar = CalculateCameraViewFar(SceneFlag);
+    CameraAngle[0] += EarthQuake;
+}
 
-        Vector(0.f, -CameraDistance, 0.f, Position);//-750
-        AngleMatrix(CameraAngle, Matrix);
-        VectorIRotate(Position, Matrix, TransformPosition);
+/**
+ * @brief Updates custom camera distance based on terrain flags.
+ */
+static void UpdateCustomCameraDistance()
+{
+    int iIndex = TERRAIN_INDEX((Hero->PositionX), (Hero->PositionY));
 
-        if (SceneFlag == MAIN_SCENE)
+    if ((TerrainWall[iIndex] & TW_CAMERA_UP) == TW_CAMERA_UP)
+    {
+        if (g_fCameraCustomDistance <= CUSTOM_CAMERA_DISTANCE1)
         {
-            g_pCatapultWindow->GetCameraPos(Position);
-        }
-        else if (CCameraMove::GetInstancePtr()->IsTourMode())
-        {
-            CCameraMove::GetInstancePtr()->UpdateTourWayPoint();
-            CCameraMove::GetInstancePtr()->GetCurrentCameraPos(Position);
-            CameraViewFar = 390.f * CCameraMove::GetInstancePtr()->GetCurrentCameraDistanceLevel();
-        }
-
-        if (g_Direction.IsDirection() && !g_Direction.m_bDownHero)
-        {
-            Hero->Object.Position[2] = 300.0f;
-            g_shCameraLevel = g_Direction.GetCameraPosition(Position);
-        }
-        else if (gMapManager.IsPKField() || IsDoppelGanger2())
-        {
-            g_shCameraLevel = 5;
-        }
-        else if (IsDoppelGanger1())
-        {
-            g_shCameraLevel = 5;
-        }
-        else g_shCameraLevel = 0;
-
-        if (CCameraMove::GetInstancePtr()->IsTourMode())
-        {
-            vec3_t temp = { 0.0f,0.0f,-100.0f };
-            VectorAdd(TransformPosition, temp, TransformPosition);
-        }
-
-        VectorAdd(Position, TransformPosition, CameraPosition);
-
-        if (gMapManager.InBattleCastle())
-        {
-            CameraPosition[2] = 255.f;//700
-        }
-        else if (!CCameraMove::GetInstancePtr()->IsTourMode())
-        {
-            CameraPosition[2] = Hero->Object.Position[2];//700
-        }
-        // Note: When in tour mode and not in battle castle, CameraPosition[2] is not set here
-
-        if ((TerrainWall[iIndex] & TW_HEIGHT) == TW_HEIGHT)
-        {
-            CameraPosition[2] = g_fSpecialHeight = 1200.f + 1;
-        }
-        CameraPosition[2] += CameraDistance - 150.f;//700
-
-        if (CCameraMove::GetInstancePtr()->IsTourMode())
-        {
-            CCameraMove::GetInstancePtr()->SetAngleFrustum(-112.5f);
-            CameraAngle[0] = CCameraMove::GetInstancePtr()->GetAngleFrustum();
-            CameraAngle[1] = 0.0f;
-            CameraAngle[2] = CCameraMove::GetInstancePtr()->GetCameraAngle();
-        }
-        else if (SceneFlag == CHARACTER_SCENE)
-        {
-            CameraAngle[0] = -84.5f;
-            CameraAngle[1] = 0.0f;
-            CameraAngle[2] = -75.0f;
-            CameraPosition[0] = 9758.93f;
-            CameraPosition[1] = 18913.11f;
-            CameraPosition[2] = 675.5f;
-        }
-        else
-        {
-            CameraAngle[0] = -48.5f;
-        }
-
-        CameraAngle[0] += EarthQuake;
-
-        if ((TerrainWall[iIndex] & TW_CAMERA_UP) == TW_CAMERA_UP)
-        {
-            if (g_fCameraCustomDistance <= CUSTOM_CAMERA_DISTANCE1)
-            {
-                g_fCameraCustomDistance += 10;
-            }
-        }
-        else
-        {
-            if (g_fCameraCustomDistance > 0)
-            {
-                g_fCameraCustomDistance -= 10;
-            }
-        }
-
-        if (g_fCameraCustomDistance != 0.f)
-        {
-            vec3_t angle = { 0.f, 0.f, -45.f };
-            Vector(0.f, g_fCameraCustomDistance, 0.f, Position);
-            AngleMatrix(angle, Matrix);
-            VectorIRotate(Position, Matrix, TransformPosition);
-            VectorAdd(CameraPosition, TransformPosition, CameraPosition);
+            g_fCameraCustomDistance += 10;
         }
     }
+    else
+    {
+        if (g_fCameraCustomDistance > 0)
+        {
+            g_fCameraCustomDistance -= 10;
+        }
+    }
+}
+
+/**
+ * @brief Updates camera distance based on world and camera level.
+ */
+static void UpdateCameraDistance()
+{
     if (gMapManager.WorldActive == 5)
     {
         CameraAngle[0] += sinf(WorldTime * 0.0005f) * 2.f;
@@ -331,6 +271,134 @@ bool MoveMainCamera()
             CameraDistance += (CameraDistanceTarget - CameraDistance) / 3;
         }
     }
+}
+
+/**
+ * @brief Sets field of view based on world and tour mode.
+ */
+static void SetCameraFOV()
+{
+    if (gMapManager.WorldActive == WD_73NEW_LOGIN_SCENE
+        && CCameraMove::GetInstancePtr()->IsTourMode())
+    {
+        CameraFOV = 65.0f;
+    }
+    else
+    {
+        CameraFOV = 30.f;
+    }
+}
+
+/**
+ * @brief Handles editor-mode camera and movement controls.
+ * @return True if editor movement occurred
+ */
+#ifdef ENABLE_EDIT2
+static bool HandleEditorMode()
+{
+    bool EditMove = false;
+
+    if (!g_pUIManager->IsInputEnable())
+    {
+        // Handle camera angle rotation
+        if (HIBYTE(GetAsyncKeyState(VK_INSERT)) == 128)
+            CameraAngle[2] += 15;
+        if (HIBYTE(GetAsyncKeyState(VK_DELETE)) == 128)
+            CameraAngle[2] -= 15;
+        if (HIBYTE(GetAsyncKeyState(VK_HOME)) == 128)
+            CameraAngle[2] = -45;
+
+        CameraAngle[2] = fmodf(CameraAngle[2] + 360.0f, 360.0f) - 360.0f;
+
+        // Handle movement input
+        vec3_t p1, p2;
+        Vector(0.f, 0.f, 0.f, p1);
+        FLOAT Velocity = TERRAIN_SCALE * 1.25f * FPS_ANIMATION_FACTOR;
+
+        struct KeyMapping {
+            int vKey;
+            float x;
+            float y;
+        };
+
+        const KeyMapping keyMappings[] = {
+            {VK_LEFT,  -Velocity, -Velocity},
+            {VK_RIGHT,  Velocity,  Velocity},
+            {VK_UP,    -Velocity,  Velocity},
+            {VK_DOWN,   Velocity, -Velocity}
+        };
+
+        for (const auto& mapping : keyMappings)
+        {
+            if (HIBYTE(GetAsyncKeyState(mapping.vKey)) == 128)
+            {
+                Vector(mapping.x, mapping.y, 0.f, p1);
+                EditMove = true;
+                break;
+            }
+        }
+
+        // Apply rotation and movement
+        glPushMatrix();
+        glLoadIdentity();
+        glRotatef(-CameraAngle[2], 0.f, 0.f, 1.f);
+        float Matrix[3][4];
+        GetOpenGLMatrix(Matrix);
+        glPopMatrix();
+        VectorRotate(p1, Matrix, p2);
+        VectorAdd(Hero->Object.Position, p2, Hero->Object.Position);
+    }
+
+    AdjustHeroHeight();
+
+    if (EditMove)
+    {
+        BYTE PathX[1];
+        BYTE PathY[1];
+        PathX[0] = (BYTE)(Hero->Object.Position[0] / TERRAIN_SCALE);
+        PathY[0] = (BYTE)(Hero->Object.Position[1] / TERRAIN_SCALE);
+
+        SendCharacterMove(Hero->Key, Hero->Object.Angle[2], 1, PathX, PathY, PathX[0], PathY[0]);
+
+        Hero->Path.PathNum = 0;
+    }
+
+    return EditMove;
+}
+#endif //ENABLE_EDIT2
+
+/**
+ * @brief Main camera controller - updates camera position, angle, and distance.
+ * @return True if camera is locked
+ */
+bool MoveMainCamera()
+{
+    bool bLockCamera = false;
+
+    SetCameraFOV();
+
+#ifdef ENABLE_EDIT2
+    HandleEditorMode();
+#endif
+
+    CameraAngle[0] = 0.f;
+    CameraAngle[1] = 0.f;
+
+    if (CameraTopViewEnable)
+    {
+        CameraViewFar = 3200.f;
+        CameraPosition[0] = Hero->Object.Position[0];
+        CameraPosition[1] = Hero->Object.Position[1];
+        CameraPosition[2] = CameraViewFar;
+    }
+    else
+    {
+        CalculateCameraPosition(CameraPosition);
+        SetCameraAngle();
+        UpdateCustomCameraDistance();
+    }
+
+    UpdateCameraDistance();
 
     return bLockCamera;
 }
