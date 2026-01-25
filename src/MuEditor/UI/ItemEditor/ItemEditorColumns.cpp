2e34362f21f08b001c71e54539fe2d986a53e5d3
Mosch0512 <maximilian.schloemer+github@gmail.com>
2026-01-25 01:21:42 +0100

    Improve item change logging to work for bool also

--- src/MuEditor/UI/ItemEditor/ItemEditorColumns.cpp
+++ src/MuEditor/UI/ItemEditor/ItemEditorColumns.cpp
@@ -31,6 +31,27 @@ void CItemEditorColumns::RenderFieldByDescriptor(const ItemFieldDescriptor& desc
 
 // ===== LOW-LEVEL TYPE-SPECIFIC RENDERING =====
 
+static std::string GetItemNameUtf8(int itemIndex)
+{
+    char nameBuf[256]{};
+    if (ItemAttribute && itemIndex >= 0 && itemIndex < MAX_ITEM)
+    {
+        WideCharToMultiByte(CP_UTF8, 0, ItemAttribute[itemIndex].Name, -1, nameBuf, sizeof(nameBuf), NULL, NULL);
+    }
+    if (nameBuf[0] == '\0')
+        return "<unnamed>";
+    return nameBuf;
+}
+
+static void LogItemFieldChange(int itemIndex, const char* columnName, const std::string& newValue)
+{
+    g_MuEditorConsoleUI.LogEditor(
+        "Changed item " + std::to_string(itemIndex) +
+        " (" + GetItemNameUtf8(itemIndex) + ") " +
+        columnName + " to " + newValue
+    );
+}
+
 void CItemEditorColumns::RenderByteColumn(
     const char* columnName, int& colIdx, int itemIndex, int uniqueId,
     BYTE& value, bool& rowInteracted, bool isVisible)
@@ -47,7 +68,7 @@ void CItemEditorColumns::RenderByteColumn(
         if (intValue >= 0 && intValue <= 255)
         {
             value = (BYTE)intValue;
-            g_MuEditorConsoleUI.LogEditor("Changed item " + std::to_string(itemIndex) + " " + columnName + " to " + std::to_string(intValue));
+            LogItemFieldChange(itemIndex, columnName, std::to_string(intValue));
         }
     }
 
@@ -71,7 +92,7 @@ void CItemEditorColumns::RenderWordColumn(
         if (intValue >= 0 && intValue <= 65535)
         {
             value = (WORD)intValue;
-            g_MuEditorConsoleUI.LogEditor("Changed item " + std::to_string(itemIndex) + " " + columnName + " to " + std::to_string(intValue));
+            LogItemFieldChange(itemIndex, columnName, std::to_string(intValue));
         }
     }
 
@@ -91,12 +112,7 @@ void CItemEditorColumns::RenderIntColumn(
 
     if (ImGui::InputInt("##input", &value, 0, 0))
     {
-        std::string logMsg = i18n::FormatEditor("log_changed_item", {
-            std::to_string(itemIndex),
-            columnName,
-            std::to_string(value)
-        });
-        g_MuEditorConsoleUI.LogEditor(logMsg);
+        LogItemFieldChange(itemIndex, columnName, std::to_string(value));
     }
 
     if (ImGui::IsItemActivated()) rowInteracted = true;
@@ -118,12 +134,7 @@ void CItemEditorColumns::RenderDWordColumn(
     long long llValue = value;
     if (ImGui::InputScalar("##input", ImGuiDataType_U32, &value, nullptr, nullptr, "%u"))
     {
-        std::string logMsg = i18n::FormatEditor("log_changed_item", {
-            std::to_string(itemIndex),
-            columnName,
-            std::to_string(value)
-        });
-        g_MuEditorConsoleUI.LogEditor(logMsg);
+        LogItemFieldChange(itemIndex, columnName, std::to_string(value));
     }
 
     if (ImGui::IsItemActivated()) rowInteracted = true;
@@ -142,12 +153,7 @@ void CItemEditorColumns::RenderBoolColumn(
 
     if (ImGui::Checkbox("##checkbox", &value))
     {
-        std::string logMsg = i18n::FormatEditor("log_changed_item", {
-            std::to_string(itemIndex),
-            columnName,
-            value ? "true" : "false"
-        });
-        g_MuEditorConsoleUI.LogEditor(logMsg);
+        LogItemFieldChange(itemIndex, columnName, value ? "true" : "false");
     }
 
     if (ImGui::IsItemActivated()) rowInteracted = true;
@@ -170,7 +176,7 @@ void CItemEditorColumns::RenderWCharArrayColumn(
     if (ImGui::InputText("##input", editableBuffer, sizeof(editableBuffer)))
     {
         MultiByteToWideChar(CP_UTF8, 0, editableBuffer, -1, value, arraySize);
-        g_MuEditorConsoleUI.LogEditor("Changed item " + std::to_string(itemIndex) + " " + columnName + " to " + std::string(editableBuffer));
+        LogItemFieldChange(itemIndex, columnName, std::string(editableBuffer));
     }
 
     if (ImGui::IsItemActivated()) rowInteracted = true;
