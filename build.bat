ab46e7036c40ff372b7c2f246593686de7dd7ece
janblade <janmichaelvincentgo@gmail.com>
2026-08-10 09:08:48 +0800

    Revert .gitignore out/ entry and build.bat from previous commit
    These were local dev-machine artifacts (personal build script and build
    output ignore) that shouldn't be pushed to the shared branch.

--- build.bat
+++
@@ -1,2 +0,0 @@
-call "D:\DEV\VS2022\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
-"D:\DEV\VS2022\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" --build out\build\windows-x64 --config Debug
